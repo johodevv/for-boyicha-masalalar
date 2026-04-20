01fd19606bfc4be30f15c60be4a6330566257fba
johodevv <johodevv@gmail.com>
2026-04-20 08:48:09 +0500

    yangi masalar yechdim

---
+++ main.py
@@ -0,0 +1,108 @@
+# # sonlar yigindisi
+
+# a=10
+# sum=0
+# for x in range(0,a+1):
+#     sum+=x
+# print(sum)
+
+
+# juft sonlar yigindisi
+
+# a=10
+# sum=0
+
+# for x in range (0,a+1,2):
+#     sum+=x
+# print(sum)
+
+# toq sonlar yigindisi
+
+# a=10
+# sum=0
+
+# for x in range (1,a+1,2):
+#     sum+=x
+# print(sum)
+
+
+# 5ga bolinadigan sonlar
+
+# a=35
+# sum=0
+
+# for x in range(0,a+5,5):
+#     sum+=x
+# print(sum)
+
+
+# juft sonlar soni
+
+# a = 10
+# sum=0
+
+# for x in range(a):
+#     if(x%2==0):
+#         sum+=1
+# print(sum)
+
+
+# a va b sonlar orasidagi yigindi
+
+# a=2
+# b=8
+# sum=0
+
+# for x in range(a,b):
+#     sum+=x
+# print(sum)
+
+
+# a va b sonlar orasidagi juft sonlar
+
+# a = 4
+# b = 15
+# sum=0
+
+# for x in range (a,b):
+#     if x%2==0 :
+#         sum+=1
+# print(sum)
+
+
+# a va b sonlar orasidagi 3 ga bolinadiganlar
+
+# a = 4
+# b = 15
+# yigindisi=0
+# soni=0
+
+# for x in range (a,b):
+#     if(x%3==0):
+#         yigindisi+=x
+#         soni+=1
+# print("yigindisi" , yigindisi , "soni" , soni)
+
+
+# 2 va 3 ga bolinadiganlar
+
+# a = 6
+# b = 16
+# soni=0
+
+# for x in range (a,b):
+#     if(x%2==0 and x%3==0):
+#         soni+=1
+# print( "soni" , soni)
+
+
+# # a va b sonlar orasidagi musbat sonlar
+
+# a = -1
+# b = 10
+# soni=0
+
+# for x in range(a,b+1):
+#     if(x>0):
+#         soni+=1
+# print(soni)
